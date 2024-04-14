c5e70640efff73e7573c80aadfc37257bbc6d31d
Erfan <eyousefifar@gmail.com>
2024-04-14 16:30:47 +0330

    fix websocket types

--- src/types/webSocket.ts
+++ src/types/webSocket.ts
@@ -23,7 +23,7 @@ export interface IWebSocketClient {
 export interface ISocket {
   ws: tWebSocket;
   lastActiveTime: number;
-  interval: NodeJS.Timer;
+  interval: NodeJS.Timeout;
 }
 
 export type tCloseEvent = CloseEvent;
